f3b3b84aac556bf1d04d3e95fbada0ffadf121d6
Cody Barber <cody.n.barber@gmail.com>
2016-05-27 14:32:44 -0400

    Added input field for number of intervals, but still working on making it functional.

--- interval.js
+++ interval.js
@@ -1,17 +1,19 @@
 $(document).ready(function() {
 
-  $('.intervalLength').submit(function(event) {
-    event.preventDefault();
-    var intervalTime = $('.exercise').val();
-    intervalTime = Number(intervalTime);
-    startClock(intervalTime);
-    var cooldownTime = $('.cooldown').val();
-    cooldownTime = Number(cooldownTime)
-    setTimeout(function() {
-      startCooldown(cooldownTime);
-    }, intervalTime * 1000 + 1000);
-  });
 
+    $('.intervalLength').submit(function(event) {
+      event.preventDefault();
+      var amount = $('.amount').val();
+      amount = Number(amount);
+        var intervalTime = $('.exercise').val();
+        intervalTime = Number(intervalTime);
+        startClock(intervalTime);
+        var cooldownTime = $('.cooldown').val();
+        cooldownTime = Number(cooldownTime)
+        setTimeout(function() {
+          startCooldown(cooldownTime);
+        }, intervalTime * 1000 + 500);
+    });
 
   function startClock(intervalTime) {
     var clock = $('.exerciseTimer').FlipClock(intervalTime, {
