4378ee877928c381080de32fa59a66944282e8f0
Cody Barber <cody.n.barber@gmail.com>
2016-05-27 15:05:45 -0400

    Fixed interval amount input. Timers will repeat the amount of times that user wants.

--- interval.js
+++ interval.js
@@ -5,15 +5,23 @@ $(document).ready(function() {
       event.preventDefault();
       var amount = $('.amount').val();
       amount = Number(amount);
-        var intervalTime = $('.exercise').val();
-        intervalTime = Number(intervalTime);
-        startClock(intervalTime);
-        var cooldownTime = $('.cooldown').val();
-        cooldownTime = Number(cooldownTime)
-        setTimeout(function() {
-          startCooldown(cooldownTime);
-        }, intervalTime * 1000 + 500);
+      cycle(amount);
     });
+
+  function cycle(amount) {
+    if (amount <= 0) return;
+    var intervalTime = $('.exercise').val();
+    intervalTime = Number(intervalTime);
+    startClock(intervalTime);
+    var cooldownTime = $('.cooldown').val();
+    cooldownTime = Number(cooldownTime)
+    setTimeout(function() {
+      startCooldown(cooldownTime);
+      setTimeout(function() {
+        cycle(amount - 1);
+      }, cooldownTime * 1000 + 500)
+    }, intervalTime * 1000 + 500);
+  }
 
   function startClock(intervalTime) {
     var clock = $('.exerciseTimer').FlipClock(intervalTime, {
