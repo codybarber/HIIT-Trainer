50bfa8d6b16d08ab6d50286432127dcc03be5685
Cody Barber <cody.n.barber@gmail.com>
2016-07-17 23:06:26 -0400

    Removed flipclock all together and added youtube fucntionality

--- js/interval.js
+++ js/interval.js
@@ -1,19 +1,6 @@
 $(document).ready(function() {
 
-  // SoundCloud API
-  SC.initialize({
-    client_id: '66d8dccff739cbb7c078f98e11993438'
-  });
-
-  $('.submit').click(function() {
-    SC.stream('/playlists/241305071')
-      .then(function(player) {
-        player.play();
-      });
-  });
-
-
-  //FlipClock Code
+  var bell = new Audio('bell.mp3');
 
   $('.intervals').submit(function(event) {
     event.preventDefault();
@@ -22,33 +9,106 @@ $(document).ready(function() {
     cycle(amount);
   });
 
+
+  function workoutCountdown(element, minutes, seconds) {
+    document.getElementById('player').volume = 1.0;
+    var time = minutes*60 + seconds;
+    var interval = setInterval(function() {
+      var el = document.getElementById(element);
+      if(time === 0) {
+        $('#exerciseTimer').addClass('animated infinite flash');
+        el.innerHTML = "Time for a break!";
+        clearInterval(interval);
+        return;
+      }
+      var minutes = Math.floor(Number( time / 60 ));
+      if (minutes < 10) minutes = "0" + minutes;
+      var seconds = time % 60;
+      if (seconds < 10) seconds = "0" + seconds;
+      var text = minutes + ':' + seconds;
+      el.innerHTML = text;
+      time--;
+    }, 1000);
+  }
+
+  function cooldownCountdown(element, minutes, seconds) {
+    document.getElementById('player').volume = 0.2;
+    var time = minutes*60 + seconds;
+    var interval = setInterval(function() {
+      var el = document.getElementById(element);
+      if(time === 0) {
+        $('#cooldownTimer').addClass('animated infinite flash');
+        el.innerHTML = "Let's Go!";
+        clearInterval(interval);
+        return;
+      }
+      var minutes = Math.floor(Number( time / 60 ));
+      if (minutes < 10) minutes = "0" + minutes;
+      var seconds = time % 60;
+      if (seconds < 10) seconds = "0" + seconds;
+      var text = minutes + ':' + seconds;
+      el.innerHTML = text;
+      time--;
+    }, 1000);
+  }
+
   function cycle(amount) {
     if (amount <= 0) return;
     var intervalTime = $('.exercise').val();
     intervalTime = Number(intervalTime);
-    startClock(intervalTime);
+    workoutCountdown('exerciseTimer', 0, intervalTime);
+    bell.play();
     var cooldownTime = $('.cooldown').val();
     cooldownTime = Number(cooldownTime);
     setTimeout(function() {
-      startCooldown(cooldownTime);
+      cooldownCountdown('cooldownTimer', 0, cooldownTime);
+      bell.play();
       setTimeout(function() {
         cycle(amount - 1);
       }, cooldownTime * 1000 + 1000);
     }, intervalTime * 1000 + 1000);
   }
 
-  function startClock(intervalTime) {
-    var clock = $('.exerciseTimer').FlipClock(intervalTime, {
-      clockFace: 'MinuteCounter',
-      countdown: true
-    });
-  }
+  // YouTube Code
+  var trapRapPlaylist = 'PLcK0neBMyFxSpYgDfKCsHRwgxlN-Tnt9D';
+  var trapPlaylist = 'PLC2tuhuSqVt2uAiAZLBE0pD2tQiabZ-5j';
+  var housePlaylist = 'PL7Vr8XNhXsJAKgFY8jlTUAgiMDByR3oaw';
+  var dubstepPlaylist = 'PLA48B3068BF5AF344';
+  var futureBassPlaylist = 'PLe8jmEHFkvsbRwwi0ode5c9iMQ2dyJU3N';
 
-  function startCooldown(cooldownTime) {
-    var clock = $('.cooldownTimer').FlipClock(cooldownTime, {
-      clockFace: 'MinuteCounter',
-      countdown: true
-    });
-  }
 
+
+  window.onYouTubeIframeAPIReady = function() {
+    $('.intervals').submit(function() {
+      event.preventDefault();
+      if ($('.genre').val() === 'trap') {
+        playlist = trapPlaylist;
+      } else if ($('.genre').val() === 'trapRap') {
+        playlist = trapRapPlaylist;
+      } else if ($('.genre').val() === 'house') {
+        playlist = housePlaylist;
+      } else if ($('.genre').val() === 'dubstep') {
+        playlist = dubstepPlaylist;
+      } else if ($('.genre').val() === 'futureBass') {
+        playlist = futureBassPlaylist;
+      }
+      var player  = new YT.Player('player', {
+        height: '50',
+        width: '50',
+        playerVars:
+        {
+          'listType': 'playlist',
+          'list': playlist
+        },
+        events:
+        {
+          'onReady': onPlayerReady
+        }
+      });
+    });
+
+    function onPlayerReady(event) {
+      event.target.playVideo();
+    }
+  };
 });
